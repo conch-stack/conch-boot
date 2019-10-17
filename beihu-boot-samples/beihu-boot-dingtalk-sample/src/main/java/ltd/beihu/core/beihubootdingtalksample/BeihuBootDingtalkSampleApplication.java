4f6673eb24b2f9e0872f703504b7d24ec157874b
zjz6b6 <407091038@qq.com>
2019-10-17 12:31:10 +0800

    add dingtalk sample

---
+++ beihu-boot-samples/beihu-boot-dingtalk-sample/src/main/java/ltd/beihu/core/beihubootdingtalksample/BeihuBootDingtalkSampleApplication.java
@@ -0,0 +1,13 @@
+package ltd.beihu.core.beihubootdingtalksample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeihuBootDingtalkSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeihuBootDingtalkSampleApplication.class, args);
+    }
+
+}
